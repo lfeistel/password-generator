0b027140935b60d93e591df92419911784b9a315
William Feistel <lee@feistel.net>
2021-10-03 13:56:14 -0500

    added code to get and validate password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
 // Assignment code here
 
+function generatePassword() {
+  var passwordLength = 0;
+  var characterSet = "";
+  var password = "";
+
+  while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    passwordLength = Number(window.prompt("Enter a password length between 8 and 128 characters:"));
+    if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+      window.alert("Please enter a valid password length.");
+    }
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +22,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
